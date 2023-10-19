a570e55997619bf8ae8fad98355c2a7da244f0e3
sere06001 <sere06001@utb.vaxjo.se>
2023-10-19 10:17:59 +0200

    Add project files.

---
+++ Uppgift 4.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett meddelande");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Hur mång steg till höger ska meddelandet flyttas?");
+            int steg = int.Parse(Console.ReadLine());
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                Console.WriteLine(meddelande[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
